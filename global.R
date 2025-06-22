7d33840a01f80aad25441464b8e9d5ad0144bf98
Mikołaj Kaczmarek <12432719+AN0DA@users.noreply.github.com>
2025-06-22 10:58:29 +0200

    remove unused prediction models helper file

--- global.R
+++ global.R
@@ -24,7 +24,6 @@ library(tibble)
 # Load helper functions
 source("helpers/data_functions.R")
 source("helpers/plotting_functions.R")
-source("helpers/prediction_models.R")
 
 # Define any global variables or helper functions
 # This file is loaded before ui.R and server.R
